7daeaf7f3802068de4d502249eaf95aec21c223f
e-soto8283 <esoto0001@mymail.lausd.net>
2026-09-21 16:09:29 -0700

    Finished cow art

---
+++ GoatPainter.java
@@ -0,0 +1,226 @@
+import org.code.neighborhood.Painter;
+
+public class GoatPainter extends PainterPlus{
+
+
+    public void paintFur() {
+        // this is the furr of the goat
+    paint("Saddlebrown");
+    move();
+    paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    
+    turnRight();
+    
+      move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+
+        turnRight();
+        
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    
+        turnRight();
+
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+
+     turnRight();
+
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    
+turnRight();
+
+     move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+      move();
+      paint("Saddlebrown");
+    
+turnRight();
+
+     move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+
+    turnRight();
+
+     move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+
+    turnRight();
+
+     move();
+      paint("Saddlebrown");
+ move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    move();
+      paint("Saddlebrown");
+    
+      turnRight();
+
+     move();
+      paint("Saddlebrown");
+ move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+    
+        turnRight();
+
+     move();
+      paint("Saddlebrown");
+ move();
+      paint("Saddlebrown");
+ move();
+      paint("Saddlebrown");
+    
+        turnRight();
+
+     move();
+      paint("Saddlebrown");
+ move();
+      paint("Saddlebrown");
+
+     turnRight();
+
+     move();
+      paint("Saddlebrown");
+     move();
+      paint("Saddlebrown");
+
+     turnRight();
+
+     move();
+      paint("Saddlebrown");
+    
+     turnRight();
+
+     move();
+      paint("Saddlebrown");
+    }
+
+   public void towardEye() {
+     // moving toward the eyes
+     turnRight();
+    move();
+    move();
+    move();
+    move();
+   turnLeft();
+    move();
+    move();
+    move();
+     turnLeft();
+    move();
+   }
+
+  public void goatEyes() {
+ // these are the eyes
+paint("Gray");
+    turnLeft();
+    move();
+    paint("DarkGray");
+    turnRight();
+    move();
+   paint("white");
+    turnRight();
+    move();
+   paint("black");
+     turnRight();
+    move();
+    turnRight();
+    move();
+    move();
+    move();
+    move();
+    move();
+    move();
+    paint("DarkGray");
+    move();
+    paint("Gray");
+      turnRight();
+    move();
+   paint("black");
+    turnRight();
+    move();
+   paint("white");
+    
+  }  
+  }
---
+++ NeighborhoodRunner.java
@@ -0,0 +1,61 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    GoatPainter myGoatPainter = new GoatPainter();
+    myGoatPainter.setPaint(1000);
+
+    
+myGoatPainter.paintFur();
+myGoatPainter.towardEye();
+myGoatPainter.goatEyes();
+
+       // the mouth
+ myGoatPainter.turnLeft();
+     myGoatPainter.move();
+     myGoatPainter.move();
+     myGoatPainter.move();
+     myGoatPainter.paint("white");
+     myGoatPainter.move();
+     myGoatPainter.paint("white");
+     myGoatPainter.move();
+      myGoatPainter.paint("white");
+      myGoatPainter.turnRight();
+     myGoatPainter.move();
+    myGoatPainter.paint("white");
+    myGoatPainter.move();
+    myGoatPainter.paint("PaleVioletRed");
+     myGoatPainter.move();
+    myGoatPainter.paint("PaleVioletRed");
+     myGoatPainter.move();
+    myGoatPainter.paint("white");
+     myGoatPainter.move();
+    myGoatPainter.paint("white");
+    myGoatPainter.turnRight();
+     myGoatPainter.move();
+    myGoatPainter.paint("white");
+    myGoatPainter.move();
+    myGoatPainter.paint("white");
+     myGoatPainter.turnRight();
+     myGoatPainter.move();
+    myGoatPainter.paint("white");
+    myGoatPainter.move();
+    myGoatPainter.paint("white");
+   myGoatPainter.move();
+    myGoatPainter.paint("white");
+    myGoatPainter.move();
+    myGoatPainter.paint("white");
+     myGoatPainter.turnRight();
+     myGoatPainter.move();
+     myGoatPainter.paint("PaleVioletRed");
+     myGoatPainter.turnRight();
+     myGoatPainter.move();
+     myGoatPainter.paint("PaleVioletRed");
+     myGoatPainter.move();
+     myGoatPainter.paint("PaleVioletRed");
+      myGoatPainter.move();
+     myGoatPainter.paint("PaleVioletRed");
+    
+  }
+  }
